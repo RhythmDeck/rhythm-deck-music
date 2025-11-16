92df49615236f9db86d9b1c033f87ee2001526bf
RhythmDeck <rhythmdeckmusic@gmail.com>
2025-11-16 11:29:30 -0600

    Fix server.js: add missing quote in require('path')

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const express = require('express');
-const path = require('path 
+const path = require('path');
 
 ');
 const { createClient } = require('@supabase/supabase-js');
